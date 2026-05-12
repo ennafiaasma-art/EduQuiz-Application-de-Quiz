d05c92317cc799bb1ae6c3f12e64355c7d4ea6e5
outamghartAchraf <outamghart2000@gmail.com>
2026-05-12 15:28:36 +0100

    add function register

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -1,20 +1,32 @@
 <?php
-
+declare(strict_types=1);
 namespace Src\Services;
+use Src\Repositories\UserRepository;
+
 require_once __DIR__ . "/../../config/DB.php";
+require_once __DIR__ . "/../Repositories/UserRepository.php";
 
 class AuthService
 {
-    private $pdo;
+    private $repo;
 
     public function __construct()
     {
-        $this->pdo = \DB::connect();
+        $this->repo = new UserRepository();
     }
 
-    public function register()
+    public function register(string $name, string $email, string $password, string $role)
     {
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        $user = new User(
+            null,
+            $name,
+            $email,
+            $hashedPassword,
+            $role
+        );
 
+        return $this->repo->create($user);
 
     }
 
