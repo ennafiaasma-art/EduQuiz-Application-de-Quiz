28c56fecef27e867bbc289da21e1592918fe463a
outamghartAchraf <outamghart2000@gmail.com>
2026-05-12 15:20:53 +0100

    add function create for register

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -1,8 +1,27 @@
 <?php
 
 namespace Src\Repositories;
+use Src\Entities\User;
+
+require_once __DIR__ . "/../../config/DB.php";
 
 class UserRepository
 {
+    private $pdo;
 
+   public function __construct() {
+       $this->pdo = \DB::connect();
+   }
+
+   public function create(User $user)
+   {
+       $stateSql = $this->db->prepare("INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?) ");
+       return $stateSql->execute([
+           $user->getName(),
+           $user->getEmail(),
+           $user->getPassword(),
+           $user->getRole()
+       ]);
+
+   }
 }
