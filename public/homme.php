513e5c68d70f790e445a1d83f64047e86986ae5c
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-13 16:02:38 +0100

    initiliser le style

---
+++ public/homme.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Page d'accueil</title>
+      <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+
+
+    
+</body>
+</html>
