02225e56240e7422dddaa45a899f4a9d89553215
jihanejador <jihanejador@gmail.com>
2026-05-13 11:21:38 +0100

    creation la function avec le construct

--- src/Entities/Question.php
+++ src/Entities/Question.php
@@ -5,4 +5,9 @@ class Question{
     private int $id;
     private string $questionText;
     private arry $answers = [];
+
+    public function __construct(int $id, string $text){
+        $this->id = $id;
+        $this->questionText= $text;
+    }
 }
