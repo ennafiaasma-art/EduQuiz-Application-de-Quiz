b966dc9e55b7028bafca403cfe96c7304f1b64a7
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-15 14:51:51 +0100

    Methode créer une nouvelle question

--- src/Respositories/QuestionRepository.php
+++ src/Respositories/QuestionRepository.php
@@ -13,5 +13,19 @@ class QuestionRepository
     public function __construct() {
         $this->pdo = \DB::connect();
     }  
+
+
+     public function create(int $quizId, string $question): int
+    {
+        $stmt = $this->pdo->prepare(
+            "INSERT INTO questions(quiz_id, question)
+             VALUES (?, ?)"
+        );
+
+        $stmt->execute([$quizId, $question]);
+
+        return (int)$this->pdo->lastInsertId();
+    }
+
     
     }
