cb53a26126ce5c0ef45dc7e37cba149c79c637b7
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-15 11:54:02 +0100

    Erreur de position dans un fichier corrigée

---
+++ src/Respositories/AnswerRepository.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Src\Repositories;
+
+require_once __DIR__ . "/../../config/DB.php";
+
+use PDO;
+
+class AnswerRepository{
+
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = \DB::connect();
+    }
+}
+
+?>
