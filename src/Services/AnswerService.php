d2ae1a05825e77f7574d3d0558233c830f2c338a
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-15 16:39:56 +0100

    Ici je crée la classe AnswerService le son contructeur

--- src/Services/AnswerService.php
+++ src/Services/AnswerService.php
@@ -7,3 +7,15 @@ namespace Src\Services;
 use Src\Repositories\AnswerRepository;
 
 require_once __DIR__ . "/../Repositories/AnswerRepository.php";
+
+
+class AnswerService
+{
+    private AnswerRepository $repo;
+
+    public function __construct()
+    {
+        $this->repo = new AnswerRepository();
+    }
+
+      }
