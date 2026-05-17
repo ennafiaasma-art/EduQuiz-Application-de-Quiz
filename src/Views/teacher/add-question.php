dca87a5c72ec5d33b915d1f89c3ac2477f8ac54f
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-17 11:05:59 +0100

    Ouvrir une session pour user et le role

--- src/Views/teacher/add-question.php
+++ src/Views/teacher/add-question.php
@@ -1,1 +1,11 @@
+<?php
+session_start();
 
+if (!isset($_SESSION['user'])) {
+    header("Location: ../auth/login.php");
+    exit;
+}
+
+if (($_SESSION['user']['role_name'] ?? '') !== 'teacher') {
+    die('Access denied');
+}
