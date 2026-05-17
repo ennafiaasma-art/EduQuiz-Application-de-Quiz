48b19e1acbc62c4ea7fece40754cc7ec100d56f7
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-17 12:20:05 +0100

    function update et le message d'alert

--- src/Views/teacher/edit-question.php
+++ src/Views/teacher/edit-question.php
@@ -16,3 +16,23 @@ require_once __DIR__ . "/../../Services/QuizService.php";
 use Src\Services\QuizService;
 
 $service = new QuizService();
+
+$message = "";
+
+$id = (int)($_GET['id'] ?? $_POST['id'] ?? 0);
+
+$quiz = $id > 0 ? $service->getQuizById($id) : null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $updated = $service->updateQuiz(
+        (int)($_POST['id'] ?? 0),
+        $_POST['title'] ?? '',
+        $_POST['description'] ?? ''
+    );
+
+    if ($updated) {
+        $message = "La question est modifier avec succé";
+    }
+    $quiz = $service->getQuizById((int)($_POST['id'] ?? 0));
+}
+?>
