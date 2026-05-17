5a2b689cb65570d350af5782e0059189bf77078c
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-17 11:49:44 +0100

    On initialise une variable vide pour  afficher un message après l’action

--- src/Views/teacher/add-question.php
+++ src/Views/teacher/add-question.php
@@ -22,3 +22,42 @@ use Src\Services\AnswerService;
 $quizService = new QuizService();
 $QuestionService = new QuestionService();
 $AnswerService = new AnswerService();
+
+
+$message = "";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $answers = $_POST['answers'] ?? [];
+    $correct = $_POST['correct'] ?? null;
+
+    $questionId = $questionService->createQuestion(
+        (int)($_POST['quiz_id'] ?? 0),
+        $_POST['question'] ?? ''
+    );
+
+    if ($questionId > 0) {
+
+        foreach ($answers as $index => $answer) {
+
+            $isCorrect = ($correct == $index);
+
+            $answerService->createAnswer(
+                $questionId,
+                $answer,
+                $isCorrect
+            );
+        }
+
+        $message = "Question added successfully";
+
+    } else {
+
+        $message = "Unable to add question";
+    }
+}
+
+$allquiz = $quizService->getTeacherQuizzes(
+    (int)($_SESSION['user']['id'] ?? 0)
+);
+?>
