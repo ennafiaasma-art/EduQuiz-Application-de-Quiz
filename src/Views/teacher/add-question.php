3715ebb9c8779105179d762305fcab043fa9fb2d
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-05-17 11:08:26 +0100

    connexion avec les autres fichiers

--- src/Views/teacher/add-question.php
+++ src/Views/teacher/add-question.php
@@ -9,3 +9,12 @@ if (!isset($_SESSION['user'])) {
 if (($_SESSION['user']['role_name'] ?? '') !== 'teacher') {
     die('Access denied');
 }
+
+require_once __DIR__ . "/../../../config/DB.php";
+require_once __DIR__ . "/../../Services/QuizService.php";
+require_once __DIR__ . "/../../Services/QuestionService.php";
+require_once __DIR__ . "/../../Services/AnswerService.php";
+
+use Src\Services\QuizService;
+use Src\Services\QuestionService;
+use Src\Services\AnswerService;
